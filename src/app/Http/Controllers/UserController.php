26611d3a22ed736aa10cb57dc9145bd70a952ae6
Matej Chlustik <matejchlustik221@gmail.com>
2023-11-08 21:12:49 +0100

    20+32+34+35-vytvaranie-uctov

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Role;
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Models\CompanyEmployee;
+use App\Models\DepartmentEmployee;
 use Illuminate\Support\Facades\Hash;
 
-class UserController extends Controller
-{
+class UserController extends Controller {
+    
     public function register(Request $request) {
         $fields = $request->validate([
             'email' => 'required|string|unique:users,email',
@@ -23,10 +25,10 @@ class UserController extends Controller
             'first_name' => $fields['first_name'],
             'email' => $fields['email'],
             'password' => Hash::make($fields['password']),
-            'role_id' => Role::firstWhere("role", "Študent")->id
+            'role_id' => Role::firstWhere("role", "Admin")->id
         ]);
 
-        $token = $user->createToken("testtoken")->plainTextToken;
+        $token = $user->createToken("adminToken, ['create-department-head,create-department-employee,create-company-representative,create-student']")->plainTextToken;
 
         return response(
             [
@@ -35,5 +37,106 @@ class UserController extends Controller
             ],
             201
         );
+    }
+
+    public function store(Request $request) {
+
+        $fields = $request->validate([
+            'email' => 'required|string|unique:users,email',
+            'role_id' => 'required',
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+        ]);
+
+        $fields['department_id'] = $request->department_id;
+        $fields['company_id'] = $request->company_id;
+        $fields['phone'] = $request->phone;
+     
+        if (($fields['company_id'] !== null && $fields['department_id'] !== null)) {
+            return response('can\'t send company_id and department_id together', 400);
+        }
+
+        switch ($fields['role_id']) {
+            case 2:
+                if($fields['department_id']) {
+                    if (auth()->user()->tokenCan('create-department-head')) {
+                        $newUser = User::create([
+                            'last_name' => $fields['last_name'],
+                            'first_name' => $fields['first_name'],
+                            'email' => $fields['email'],
+                            'password' => Hash::make('123'),
+                            'role_id' => $fields['role_id'],
+                        ]);
+
+                        DepartmentEmployee::create([
+                            'user_id' => $newUser->id,
+                            'department_id' => $fields['department_id']
+                        ]);
+
+                        return response('User created', 201);
+                    } else return response('Unauthorized', 401);
+                } else return response("Missing department_id", 400);
+                break;
+
+            case 3:
+                if($fields['department_id'] !== null) {
+                    if (auth()->user()->tokenCan('create-department-head')) {
+                        $newUser = User::create([
+                            'last_name' => $fields['last_name'],
+                            'first_name' => $fields['first_name'],
+                            'email' => $fields['email'],
+                            'password' => Hash::make('123'),
+                            'role_id' => $fields['role_id'],
+                        ]);
+
+                        DepartmentEmployee::create([
+                            'user_id' => $newUser->id,
+                            'department_id' => $fields['department_id']
+                        ]);
+
+                        return response('User created', 201);
+                    } else return response('Unauthorized', 401);
+                } else return response("Missing department_id", 400);
+                break;
+
+            case 4:
+                if($fields['company_id'] !== null && $fields['phone'] !== null) {
+                    if (auth()->user()->tokenCan('create-company-representative')) {
+                        $newUser = User::create([
+                            'last_name' => $fields['last_name'],
+                            'first_name' => $fields['first_name'],
+                            'email' => $fields['email'],
+                            'password' => Hash::make('123'),
+                            'role_id' => $fields['role_id'],
+                        ]);
+
+                        CompanyEmployee::create([
+                            'user_id' => $newUser->id,
+                            'company_id' => $fields['company_id'],
+                            'phone' => $fields['phone']
+                        ]);
+
+                        return response('User created', 201);
+                    } else return response('Unauthorized', 401);
+                } else return response("Missing company_id or phone", 400);
+                break;
+
+            case 5:
+                if (auth()->user()->tokenCan('create-student')) {
+                    $newUser = User::create([
+                        'last_name' => $fields['last_name'],
+                        'first_name' => $fields['first_name'],
+                        'email' => $fields['email'],
+                        'password' => Hash::make('123'),
+                        'role_id' => $fields['role_id'],
+                    ]);
+
+                    return response('User created', 201);
+                } else return response('Unauthorized', 401);
+                break;
+
+            default:
+                return response('Wrong role_id', 401);
+        }
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -15,12 +15,22 @@ use App\Http\Controllers\CompanyController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
 Route::post("/register", [UserController::class, "register"]);
+
 Route::put("/companies/{company}", [CompanyController::class, "update"]);
+
 Route::delete("/companies/{company}", [CompanyController::class, "destroy"]);
+
 Route::get("/companies", [CompanyController::class, "index"]);
+
 Route::get("/companies/{company}", [CompanyController::class, "show"]);
+
 Route::post("/companies", [CompanyController::class, "store"]);
-Route::middleware('auth:sanctum')->get('/testing', function (Request $request) {
-    return "hello";
+
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post("/users", [UserController::class, "store"])->middleware(
+        'ability:create-department-head,create-department-employee,create-company-representative,create-student'
+    );
 });
