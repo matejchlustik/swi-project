c67598ff3bbefefeefba083200c5dc20325c9a82
Matej Chlustik <matejchlustik221@gmail.com>
2023-11-30 08:36:25 +0100

    Fix User model import

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -11,7 +11,6 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
